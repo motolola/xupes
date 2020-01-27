dad83871b6c815cd84f5340e5322925b9f170bc2
Akin Motolola <akin.mototola@xupes.com>
2020-01-27 13:03:39 +0000

    Just trying a client on my local.

--- src/main/java/com/xupes/randomhouse/RandomhouseApplication.java
+++ src/main/java/com/xupes/randomhouse/RandomhouseApplication.java
@@ -1,13 +1,30 @@
 package com.xupes.randomhouse;
 
+import com.xupes.randomhouse.xupeAipClient.Client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Date;
 
 @SpringBootApplication
-public class RandomhouseApplication {
-
-	public static void main(String[] args) {
+public class RandomhouseApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(RandomhouseApplication.class, args);
+		System.out.println(Client.getToken());
 	}
 
+	@Scheduled(initialDelay = 1000L, fixedDelay = 500L)
+	public void testing()
+	{
+		//System.out.println(new Date());
+	}
+
+	@Configuration
+	@EnableScheduling
+	class schedulinh { }
 }
---
+++ src/main/java/com/xupes/randomhouse/xupeAipClient/Client.java
@@ -0,0 +1,32 @@
+package com.xupes.randomhouse.xupeAipClient;
+
+
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+public class Client
+{
+    final static String URL_XUPES = "https://localhost/";
+
+    public static String getToken()
+    {
+        String username = "akin.motolola@xupes.com";
+        String secret = "password";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+        requestBody.add("userName", username);
+        requestBody.add("secret", secret);
+
+        HttpEntity<?> httpEntity = new HttpEntity<>(requestBody, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response = restTemplate.exchange(URL_XUPES + "token", HttpMethod.POST, httpEntity, String.class);
+        return response.getBody();
+    }
+}
